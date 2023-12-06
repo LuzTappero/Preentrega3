e2af8b29bc0cf1514bfd74407240e9d671eae987
Luz.Tappero <luztappero@gmail.com>
2023-12-06 19:39:42 -0300

    6/12 19:40

--- Products/urls.py
+++ Products/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path ('Products/', views.Products, name='Products'),
     path ('Products/crear/', views.crear_productos, name="crear_productos"), # type: ignore
     path ('Products/buscar/', views.buscar_productos , name="buscar_productos"),  # type: ignore
-    # path ('Products/crearUsuario/', views.crear_usuario, name="crearUsuario"), # type: ignore
     path ('Products/crearComentario/', views.crear_comentario , name= "crear_comentario"),# type: ignore
     path ('Products/buscarComentario/', views.buscar_comentario, name="buscar_comentario"),# type: ignore
-    path ('Products/crearCategorias/', views.crear_categoria_producto, name="crear_categoria"),# type: ignore
+    path ('Products/crearCategorias/', views.crear_categoria_producto, name="crear_categoria"), #type: ignore
 ]
